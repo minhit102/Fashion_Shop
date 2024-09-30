67bd3769816e0b03c412d4b406f98468bf2ddc48
minhit102 <minhit102k66@gmail.com>
2024-09-30 20:27:44 +0700

    add uploadImage

---
+++ src/middleware/uploadImage.js
@@ -0,0 +1,58 @@
+const multer = require('multer');
+const path = require('path');
+
+const MAX_SIZE = 5 * 1024 * 1024; // 5mb
+const storageImage = multer.diskStorage({
+    destination: async function (req, file, cb) {
+        if (file.fieldname === 'mainImage') {
+            cb(null, 'src/uploads/mainImage');
+        }
+        if (file.fieldname === 'productImage') {
+            cb(null, 'src/uploads/productImage');
+        }
+        if (file.fieldname === 'reviewImage') {
+            cb(null, 'src/uploads/reviewImage');
+        }
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.nameProduct + '-' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        let size = +req.rawHeaders.slice(-1)[0];
+
+        if (size >= MAX_SIZE) {
+            let error = [
+                {
+                    param: file.fieldname,
+                    msg: 'file_too_large',
+                },
+            ];
+            cb(null, false);
+        } else {
+            cb(null, true);
+        }
+    } else {
+        let error = [
+            {
+                param: file.fieldname,
+                msg: 'invalid_file_type',
+            },
+        ];
+        cb(null, false);
+    }
+};
+
+const uploadImage = multer({
+    limits: { fileSize: MAX_SIZE },
+    fileFilter: fileFilter,
+    storage: storageImage,
+});
+
+module.exports = { uploadImage };
